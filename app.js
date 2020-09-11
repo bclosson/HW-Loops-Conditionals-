07b0f8fba8b049ce7bb93f8926ed78c949ae9f0e
benjaminclosson1 <benjaminclosson1@gmail.com>
2020-09-10 20:57:23 -0700

    Commit 2 - Get Even answered

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-console.log('js is working');
+// console.log('js is working');
 
-// Easy Going
-// 1)
-for (let i = 1; i < 21; i++) {
+// // Easy Going
+// // 1)
+// for (let i = 1; i < 21; i++) {
+//     console.log(i);
+// }
+
+// Get Even
+// 2)
+
+for (let i = 0; i <= 200; i += 2) {
     console.log(i);
 }
