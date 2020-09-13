78b7fa07f840d2bbace36f4273faeb2becd5c5a9
benjaminclosson1 <benjaminclosson1@gmail.com>
2020-09-12 17:20:19 -0700

    Commit 5 - Getting to Know You answered

--- app.js
+++ app.js
@@ -74,14 +74,81 @@
 // &#x1F534; The commit message should read: <br>
 // "Commit 4 - Fizz Buzz answered"
 
-for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log('FizzBuzz');
-    }else if (i % 3 === 0) {
-        console.log('Fizz')
-    }else if (i % 5 === 0) {
-        console.log('Buzz');
-    }else {
-        console.log(i);
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log('FizzBuzz');
+//     }else if (i % 3 === 0) {
+//         console.log('Fizz')
+//     }else if (i % 5 === 0) {
+//         console.log('Buzz');
+//     }else {
+//         console.log(i);
+//     }
+// }
+
+// 5)
+
+
+
+// ## Getting to Know You
+// Use the following arrays to answer the questions below (name, age, hometown):
+// ```javascript
+const kenny = ["Kenny", 1000, "Austin"];
+const jimHaff = ["Jim H", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const jimClark = ["Jim C", 186, "LA"];
+const ryan = ["Ryan", 65, "Denver"];
+// ```
+// 1. Jim Clark decides that Kenny can't be named "Kenny" anymore. Remove "Kenny" from the `kenny` array and replace it with "Gameboy".
+ 
+//1)
+kenny[0] = "Gameboy";
+  kenny.splice(0, 1, "Gameboy");
+  console.log(kenny);
+// 2. Jim Clark just had his birthday; change jimClark's array to reflect him being a year older.  Don't just hard code `187`--pretend that you didn't already know that his age is 186, and write your code to just make him a year older than whatever age he was.
+
+// 2)
+
+for (let i = 0; i < jimClark.length; i++) {
+    if (typeof jimClark[i] === "number") {
+        let age = jimClark[i] + 1;
+        jimClark.splice(1, 1, age);
+        console.log(jimClark)
     }
 }
+
+// 3. Ryan is Batman maybe. Or possibly Robin. Change Ryan's hometown from "Denver" to "Gotham City".
+
+// 3)
+
+ryan.splice(2, 1, "Gotham");
+ console.log(ryan);
+
+// 4. Reuben left Durham 5 years ago to come to Chicago. First, remove "Durham" from Reuben's array, and then add "Chicago". (Note: remove and then add is different from simply changing the value at that index.)
+
+// 4)
+
+reuben.pop();
+console.log(reuben);
+
+reuben.push("Chicago");
+console.log(reuben);
+
+// 5. Jim Haff could be literally anywhere in the world. Remove "All cities" from his array, then pick any 3 cities you like, and add them to Jim's array. If you did it in 3 lines of code that's fine, but [see if you can do it in one line of code](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push)
+
+// 5)
+
+jimHaff.splice(0, 3, "Paris", "Tokyo", "Prague");
+console.log(jimHaff);
+
+
+
+// 6. Bonus: Whoops! Jim Haff is actually only allowed to be in one of two cities.  Whatever the _first_ of the 3 cities for Jim Haff is now, remove it from the array using [`.splice()`](https://www.google.com/search?q=mdn+splice&oq=mdn+splice&aqs=chrome.0.0j69i60j0l4.1078j0j7&sourceid=chrome&ie=UTF-8)
+
+// 6) 
+
+jimHaff.splice(0,1)
+console.log(jimHaff);
+// <hr>
+// &#x1F534; The commit message should read: <br>
+// "Commit 5 - Getting to Know You answered"
